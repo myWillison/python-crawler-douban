f9d5334d04858cc1836274a70e3d38ddfe6068e6
zlikun <zlikun-dev@hotmail.com>
2018-07-18 21:33:52 +0800

    实现列表页解析逻辑

--- douban/spiders/book_spider.py
+++ douban/spiders/book_spider.py
@@ -1,12 +1,41 @@
 # -*- coding: utf-8 -*-
+import scrapy
 from scrapy import Spider
 
 
 class BookSpider(Spider):
     name = 'book_spider'
 
+    allowed_domains = ['book.douban.com']
+
     def start_requests(self):
-        pass
+        """
+        Top250列表页非常规律，每页25条记录，所以后一页查询参数为当前页查询参数+25
+        :return:
+        """
+        for i in range(0, 250, 25):
+            yield scrapy.Request('https://book.douban.com/top250?start={}'.format(i), self.parse)
 
     def parse(self, response):
+        """
+        解析书籍列表页
+        :param response:
+        :return:
+        """
+        yield from [response.follow(url, self.parse_subject) for url in
+                    response.xpath('//a[@class="nbg"]/@href').extract()]
+
+        pass
+
+    def parse_subject(self, response):
+        """
+        解析书籍详情页
+        :param response:
+        :return:
+        """
+        # TODO 提取书籍信息
+
+
+        # TODO 组装书评页请求
+
         pass
